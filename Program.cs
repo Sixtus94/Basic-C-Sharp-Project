6504011f904069db62ddf641d69c9c64ab7eb4fc
Sixtus94 <sixtech39@hotmail.com>
2025-10-26 13:29:36 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AcmeAccountingSystemProject.cs
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Acme Accounting System");
+            Console.WriteLine("Remember, we're \  "accounting\" on you!");
+            Console.ReadLine(); // This is to keep the console window open
+
+        }
+    }
+}
