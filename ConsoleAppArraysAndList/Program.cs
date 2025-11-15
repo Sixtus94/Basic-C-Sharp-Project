0aa76d69b961348a5cb799f3767ee88a0471c597
Sixtus94 <sixtech39@hotmail.com>
2025-11-15 20:01:57 +0000

    Add file

---
+++ ConsoleAppArraysAndList/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppArraysAndList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array of strings
+            string[] appArray = { "Safari", "Google", "Edge", "YouTube", "WhatsApp", "Facebook", "Gmail" };
+            //Ask the user for a number to display the string at the index
+            Console.WriteLine("Select a number between 0 and 7    : ");
+            int stringSelect = Convert.ToInt32(Console.ReadLine());
+            bool validstring = false;
+
+            while (!validstring)
+            {
+                try
+                {
+                    Console.WriteLine("Your favorite app is " + appArray[stringSelect]);
+                    validstring = true;
+                }
+                //This message is to display if the user pick an index which doesn't exist
+                catch
+                {
+                    Console.WriteLine("Sorry, the selected number is invalid. Please select a number between 0 and 7.");
+                    stringSelect = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            // List of Strings
+            List<string> occupationList = new List<string>()
+             {
+             "Accountant",
+             "Doctor",
+             "Artist",
+             "Lecturer",
+             "Software Developer",
+             "UX Designer",
+             "Podcast Host",
+             "QA Tester",
+             "Lawyer"
+             };
+
+            //Ask the user for a number to display the string at a particular index
+            Console.WriteLine("\nSelect another number between 0 and 9:");
+            int listSelect = Convert.ToInt32(Console.ReadLine());
+            bool validList = false;
+
+            while (!validList)
+
+            {
+                try
+                {
+                    Console.WriteLine("Your new occupation is: " + occupationList[listSelect]);
+                    validList = true;
+                }
+
+                // Add a message to display if the user picks an index that doesn't exist
+                catch
+                {
+                    Console.WriteLine("Sorry, that number selection is invalid. select a number between 0 and 9.");
+                    listSelect = Convert.ToInt32(Console.ReadLine());
+                }
+                
+            }
+
+
+
+        }
+    }
+}
--- ConsoleAppArraysAndList/Properties/AssemblyInfo.cs
+++ ConsoleAppArraysAndList/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ArrayAndList")]
+[assembly: AssemblyTitle("ConsoleAppArraysAndList")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ArrayAndList")]
+[assembly: AssemblyProduct("ConsoleAppArraysAndList")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e5ecf100-6132-438c-b1e6-b635aa96a433")]
+[assembly: Guid("6bc1407f-0bf4-4827-8d94-96e97fbf5838")]
 
 // Version information for an assembly consists of the following four values:
 //
