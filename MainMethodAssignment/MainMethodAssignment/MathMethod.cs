6b6c1dd9873f43599ed007163bd688c158f32713
Sixtus94 <sixtech39@hotmail.com>
2025-12-01 16:18:49 +0000

    add file

---
+++ MainMethodAssignment/MainMethodAssignment/MathMethod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MathMethod
+    {       // The MathMethods Class
+
+        //This code defines a MathMethod with three over loaded MathOp that take an integer, a decimal and a string as input
+        //Each method performs different math operation and returns the result as an integer.
+
+        // Define a class to perform math operations
+        public class MathMethods
+        {
+            // This First method takes an integer
+            public int Method(int num)
+            {
+                // Return the input number 
+                return num + 5;
+            }
+
+            // Second Method triple a decimal and round to the nearest integer
+            public int Method(decimal num)
+            {
+                // convert the decimal to an int to perform math operation
+                int newnum = Convert.ToInt32(num);
+                return newnum * 10;
+            }
+
+            // Method to add 5 to a string that can be converted to an integer
+            public int Method(string numStr)
+            {
+                // convert the string to int and perform math operation
+                int newnum = Convert.ToInt32(numStr);
+                return newnum % 7;
+            
+            }
+            
+        }
+    }
+ } 
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main MathMethod part 2
+            // Instantiating MathMethod.MathMethods class
+            MathMethod.MathMethods math1 = new MathMethod.MathMethods();
+
+            // Call the Method with an integer and display the result
+            Console.WriteLine(math1.Method(2));
+
+            // Call the Method with a decimal and display the result
+            Console.WriteLine(math1.Method(3.2m));
+
+            // Call the Method with a string and display the result
+            Console.WriteLine(math1.Method("5"));
+        }
+    }
+
+
+}
+    
+
