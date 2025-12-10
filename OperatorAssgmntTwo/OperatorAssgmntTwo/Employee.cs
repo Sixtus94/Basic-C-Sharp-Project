a1860990ef16ee461b25ed533b329d2d22d79262
Sixtus94 <sixtech39@hotmail.com>
2025-12-10 11:06:44 +0000

    Add file

---
+++ OperatorAssgmntTwo/OperatorAssgmntTwo/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssgmntTwo
+{
+    class Employee
+    { 
+        // The Employee class represents a worker with an Id, FirstName, and LastName
+        public int Id { get; set; }   // This Property store the employee's unique identifier: Id
+        public string FirstName { get; set; }  // This Property store the employee's first name
+        public string LastName { get; set; }  // This Property store the employee's last name
+
+        // Overloading the == operator to compare two Employee objects by their Id
+        public static bool operator ==(Employee employ1, Employee employ2)
+        {
+            // If both references are null, they are considered equal
+            if (ReferenceEquals(employ1, employ2))
+                return true;
+
+            // If one is null but not the other, they are not equal
+            if (employ1 is null || employ2 is null)
+                return false;
+
+            // Compare employees by their Id property
+            return employ1.Id == employ2.Id;
+        }
+
+        // Overloading the != operator to match the == operator
+        public static bool operator !=(Employee employ1, Employee employ2)
+        {
+            return !(employ1 == employ2);
+        }
+
+        // Override Equals and  match operator overloads
+        public override bool Equals(Object obj)
+        {
+            if (obj is Employee other)
+                return this == other;
+            return false;
+        }
+    }
+}
---
+++ OperatorAssgmntTwo/OperatorAssgmntTwo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssgmntTwo
+{
+    class Program
+    {
+            // The entry point of the console application
+            static void Main(string[] args)
+            {
+               
+                // Create first Employee object and assign its properties
+                Employee employ1 = new Employee { Id = 1, FirstName = "John", LastName = "Mark" };
+
+                // Create second Employee object with the same Id (This is considered equal)
+                Employee employ2 = new Employee { Id = 1, FirstName = "Jane", LastName = "Kelly" };
+
+                // Create third Employee object with a different Id (so it is not equal to employee1)
+                Employee employ3 = new Employee { Id = 2, FirstName = "Bob", LastName = "Brown" };
+
+                // Compare employee1 and employee2 using the overloaded == operator
+                Console.WriteLine("Output :" + (employ1 == employ2));
+
+                // Compare emp1 and emp3 using the overloaded == operator
+                Console.WriteLine("Output : " + (employ1 == employ3));
+                Console.ReadKey();
+
+            }
+    }
+}
