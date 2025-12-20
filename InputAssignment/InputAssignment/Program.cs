6dcc5b4754525fdbeaeb59922fc8d638519b58d3
Sixtus94 <sixtech39@hotmail.com>
2025-12-20 14:49:07 +0000

    Add Input File

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+// Define the namespace the input console application
+namespace InputAssignment
+{    
+    //Define the program class
+    class Program
+    {   
+        // Define the main entry point for the console input application
+        static void Main(string[] args)
+        {
+            // Ask the user for a number
+            Console.Write("Please enter a number: ");
+
+            // Read the user's input and store it in a string variable
+            string userInput = Console.ReadLine();
+
+            // Log the number to a text file
+            LogNumberToFile(userInput, @"C:\Users\sixte\logs\log.txt");
+
+            // Print the text file back to the user
+            PrintFileContents();
+        }
+        // Method to log the number to a text file
+        static void LogNumberToFile(string number, string filePath)
+        {
+            try
+            {
+               
+                // Use StreamWriter to write to the file, overwriting any existing content
+                using (StreamWriter writer = new StreamWriter("number.txt"))
+                {
+                    // Write the number to the file
+                    writer.WriteLine(number);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur during file writing
+                Console.WriteLine("An error occurred while writing to the file: " + ex.Message);
+            }
+        }
+            // Method to print the text file contents
+            static void PrintFileContents()
+            {
+                try
+                {
+                    // Specify the file path and name
+                    string filePath = "number.txt";
+
+                    // Check if the file exists
+                    if (File.Exists(filePath))
+                    {
+                        // Read all lines from the file
+                        string[] lines = File.ReadAllLines(filePath);
+
+                        // Print each line to the console
+                        foreach (string line in lines)
+                        {
+                            Console.WriteLine("You entered: " + line);
+                        }
+                    }
+                    else
+                    {
+                        // Handle the case where the file does not exist
+                        Console.WriteLine("The file does not exist.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle any exceptions that occur during file reading
+                    Console.WriteLine("An error occurred while reading the file: " + ex.Message);
+                }
+            }
+        
+    }
+}
---
+++ InputAssignment/InputAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("InputAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InputAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("290edf4f-5889-4b94-b4b3-695de713cf94")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
