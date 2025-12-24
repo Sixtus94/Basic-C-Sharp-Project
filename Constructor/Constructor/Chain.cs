5016ab588c26117d427e2b99b8da501cd5a69924
Sixtus94 <sixtech39@hotmail.com>
2025-12-24 16:15:02 +0000

    Add Constructorb Assignment

---
+++ Constructor/Constructor/Chain.cs
@@ -0,0 +1,28 @@
+﻿// Constructor Assignment
+
+using System;
+
+namespace Constructor
+{
+    class Chain
+        // Constructor if one parameter, a float, is provided
+    {
+        public Chain(float num1) : this(num1, "Mosad")
+        {
+            Console.WriteLine(num1 + " your default name is  " + Name);
+        }
+        // Constructor if one parameter, a string, is provided
+        public Chain(string name1) : this(42, name1)
+        {
+            Console.WriteLine(name1 + " and your default favorite number is " + Num);
+        }
+        // Constructor if both parameters, a string and a float, are provided
+        public Chain(float num, string name)
+        {
+            Name = name;
+            Num = num;
+        }
+        string Name { get; set; }
+        float Num { get; set; }
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Constructor Assignment
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a const variable
+            const string const1 = "Your name is: ";
+            const string const2 = "Your favorite number is:";
+            Console.WriteLine("Enter your name  or favorite number: ");
+            // create a variable using the var keyword
+            var userInput = Console.ReadLine();
+            if (float.TryParse(userInput, out float x)) //if the user input is a number
+            {
+                Console.WriteLine(const2);    // print the favorite number message 
+                Chain newChain = new Chain(x); // Create a new chain object using the number constructor
+                Console.ReadLine();     // wait for the user to press enter before closing
+            }
+            else           //if the user input is a string
+            {
+                Console.WriteLine(const1);  // print the name messge to the console
+                Chain newChain = new Chain(userInput); // Create a new chain object using the string constructor
+                Console.ReadLine();
+            }
+        }
+    }
+}
